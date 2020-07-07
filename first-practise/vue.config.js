50857162190076ebb55c8511164055cca34a16dc
Averkin Evgeniy <Evgeniy.Averkin@t-systems.ru>
2020-07-07 10:44:56 +0300

    1st practise: Created base project with vue-cli, added some configurations inside vue.config.js

---
+++ first-practise/vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+    //Base Path
+    publicPath: '/test-path',
+    
+    // Output directory
+    outputDir: 'test-output',
+    
+    // Directory for static resources
+    assetsDir: 'test-assets',
+    
+    // Output path for html
+    indexPath: 'index.html'
+
+}
